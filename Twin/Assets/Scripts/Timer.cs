73c96da6f630d12f860f30cbb66bd0228bdfb42a
Viacheslav Born <64132803+TABASCO1990@users.noreply.github.com>
2023-07-18 17:25:29 +0500

    Add Timer

---
+++ Twin/Assets/Scripts/Timer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private Image _fill;
+    [SerializeField] private TMP_Text _time;
+    [SerializeField] private int _duration;
+    [SerializeField] private float _remainingDuration;
+
+    private void Awake()
+    {
+        BeginTime(_duration);
+    }
+
+    private void Update()
+    {
+        UpdateTimer();
+    }
+
+
+    private void UpdateTimer()
+    {
+        if (_remainingDuration >= 0)
+        {
+            _remainingDuration -= Time.deltaTime;
+            _time.text = Mathf.Round(_remainingDuration).ToString();
+            _fill.fillAmount = Mathf.InverseLerp(0, _duration, _remainingDuration);
+        }
+        else
+        {
+            EndTime();
+        }     
+    }
+
+    private void BeginTime(int second)
+    {
+        _remainingDuration = second;
+    }
+
+    private void EndTime()
+    {
+        print("Конец");
+    }
+}
