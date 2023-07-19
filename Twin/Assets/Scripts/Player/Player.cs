9107e764cbecb4250022045c65ea083e7fd55116
Viacheslav Born <64132803+TABASCO1990@users.noreply.github.com>
2023-07-19 13:31:55 +0500

    Add Score

--- Twin/Assets/Scripts/Player/Player.cs
+++ Twin/Assets/Scripts/Player/Player.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private int _bonus;
 
+    private int _score;
+    private bool isLive;
+
+    public event UnityAction<int> ScoreChanged;
+
+    public void IncreaseScore()
+    {
+        _score += _bonus;
+        ScoreChanged?.Invoke(_score);
+    }
+
+    private void TakeDamage()
+    {
+
+    }
 }
--- Twin/Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Twin/Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Player))]
 public class PlayerCollisionHandler : MonoBehaviour
 {
     [SerializeField] private UnityEvent LevelChanged;
+
+    private Player _player;
+
+    private void Start()
+    {
+        _player = GetComponent<Player>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent(out Target target))
         {
+            _player.IncreaseScore();
             LevelChanged?.Invoke();
         }
     }
