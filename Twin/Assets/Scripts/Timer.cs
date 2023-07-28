049edee533202031cf753b56d368692e04df0dc5
Viacheslav Born <64132803+TABASCO1990@users.noreply.github.com>
2023-07-28 17:29:19 +0500

    Update UI

--- Twin/Assets/Scripts/Timer.cs
+++ Twin/Assets/Scripts/Timer.cs
@@ -4,11 +4,17 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
+    private const float TimeBeLow = 10f;
+    private const float TimeLow = 5f;
+
     [SerializeField] private Image _fill;
     [SerializeField] private TMP_Text _time;
+    [SerializeField] private Color _colorLowTime;
     [SerializeField] private int _duration;
     [SerializeField] private float _remainingDuration;
     [SerializeField] private Player _player;
+
+    private Color _startColorTimer;
 
     private void OnEnable()
     {
@@ -22,6 +28,8 @@ public class Timer : MonoBehaviour
 
     private void Awake()
     {
+        _startColorTimer = _fill.color;
+        ChangeTimeColor(_startColorTimer);
         BeginTime(_duration);
     }
 
@@ -33,6 +41,7 @@ public class Timer : MonoBehaviour
     public void ResetTime()
     {
         _remainingDuration = _duration;
+        ChangeTimeColor(_startColorTimer);
     }
 
     private void BeginTime(int second)
@@ -47,26 +56,40 @@ public class Timer : MonoBehaviour
             _remainingDuration -= Time.deltaTime;
             _time.text = Mathf.Round(_remainingDuration).ToString();
             _fill.fillAmount = Mathf.InverseLerp(0, _duration, _remainingDuration);
+            SetColor();
         }
         else
         {
             EndTime();
-        }     
+        }
     }
 
     private void EndTime()
     {
-        if(_remainingDuration <= 0)
-        {
+        if (_remainingDuration <= 0)
             _player.Die();
-        }
     }
 
     private void OnTimeChanged(float time)
     {
-        if (_remainingDuration + time > _duration)       
-            _remainingDuration = _duration;   
+        if (_remainingDuration + time > _duration)
+            _remainingDuration = _duration;
         else
             _remainingDuration += time;
     }
+
+    private void SetColor()
+    {
+        if (_time.text == TimeBeLow.ToString())
+            ChangeTimeColor(Color.yellow);
+        else if(_time.text == TimeLow.ToString())
+            ChangeTimeColor(_colorLowTime);
+    }
+
+    private void ChangeTimeColor(Color color)
+    {
+        _time.color = color;
+        _fill.color = color;
+    }
 }
+
