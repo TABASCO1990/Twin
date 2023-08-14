9fff83cbbbf033eac8a6a1379f513185289d0614
Viacheslav Born <64132803+TABASCO1990@users.noreply.github.com>
2023-08-14 19:53:07 +0500

    Add language

--- Twin/Assets/Scripts/UI/LanguageSwitch.cs
+++ Twin/Assets/Scripts/UI/LanguageSwitch.cs
@@ -1,41 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Lean.Localization;
 
 public class LanguageSwitch : MonoBehaviour
 {
+    const int LanguageRussian = 0;
+    const int LanguageEnglish = 1;
+
     [SerializeField] private Sprite[] _flags;
     [SerializeField] private Image _imageFlag;
+
+    private string _languageRussian = "Russian";
+    private string _languageEnglish = "English";
+    private string _languageTurkish = "Turkish";  
 
     private int indexFlag;
 
     private void Start()
     {
         indexFlag = 0;
+        _imageFlag.sprite = _flags[indexFlag];       
     }
 
     public void NextLanguage()
     {
         indexFlag++;
 
-        for (int i = 0; i < _flags.Length; i++)
+        if (indexFlag > _flags.Length - 1)
         {
-            _imageFlag.sprite = _flags[indexFlag];
+            indexFlag = 0;
         }
 
-        print(indexFlag);
+        _imageFlag.sprite = _flags[indexFlag];
+        SetLanguage();
     }
 
     public void PreviousLanguage()
     {
         indexFlag--;
 
-        for (int i = 0; i < _flags.Length; i++)
+        if (indexFlag < 0)
         {
-            _imageFlag.sprite = _flags[indexFlag];
+            indexFlag = _flags.Length - 1;
         }
 
-        print(indexFlag);
+        _imageFlag.sprite = _flags[indexFlag];
+        SetLanguage();
+    }
+
+    private void SetLanguage()
+    {
+        switch (indexFlag)
+        {
+            case LanguageRussian:
+                LeanLocalization.SetCurrentLanguageAll(_languageRussian);
+                break;
+            case LanguageEnglish:
+                LeanLocalization.SetCurrentLanguageAll(_languageEnglish);
+                break;
+            default:
+                LeanLocalization.SetCurrentLanguageAll(_languageTurkish);
+                break;
+        }
     }
 }
