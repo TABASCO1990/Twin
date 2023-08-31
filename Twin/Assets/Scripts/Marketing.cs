7c0d96b27a4b287e28196fe64355928348229194
Viacheslav Born <64132803+TABASCO1990@users.noreply.github.com>
2023-08-31 19:41:03 +0500

    Add adv reward

--- Twin/Assets/Scripts/Marketing.cs
+++ Twin/Assets/Scripts/Marketing.cs
@@ -3,11 +3,27 @@ using System.Runtime.InteropServices;
 
 public class Marketing : MonoBehaviour
 {
+    [SerializeField] private Player _player;
+    [SerializeField] private int _seconds;
+
     [DllImport("__Internal")]
     private static extern void ShowFullScreenAdv();
+
+    [DllImport("__Internal")]
+    private static extern void AddTimeForAdv(int value); 
 
     public void ShowFullScreenAdvertisement()
     {
         ShowFullScreenAdv();
+    }
+
+    public void ShowAdvReward()
+    {
+        AddTimeForAdv(_seconds);
+    }
+
+    public void AddTime(int value)
+    {
+        _player.IncreaseTime(value);
     }
 }
--- Twin/Assets/Yandex/Plugins/my.jslib
+++ Twin/Assets/Yandex/Plugins/my.jslib
@@ -59,4 +59,24 @@ mergeInto(LibraryManager.library, {
 			}
 		})
 	},
+
+	AddTimeForAdv: function(value){
+		ysdk.adv.showRewardedVideo({
+			callbacks: {
+				onOpen: () => {
+					console.log('Video ad open.');
+				},
+				onRewarded: () => {
+					console.log('Rewarded!');
+					myGameInstance.SendMessage('Yandex','AddTime',value);
+				},
+				onClose: () => {
+					console.log('Video ad closed.');
+				}, 
+				onError: (e) => {
+					console.log('Error while open video ad:', e);
+				}
+			}
+		})
+	},
 });
