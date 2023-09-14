5f0b39c81d61f1c58ff6413431df39e1afacb7d5
Viacheslav Born <64132803+TABASCO1990@users.noreply.github.com>
2023-09-14 20:56:45 +0500

    Update

--- Twin/Assets/Scripts/FocusScreen.cs
+++ Twin/Assets/Scripts/FocusScreen.cs
@@ -4,12 +4,12 @@ public class FocusScreen : MonoBehaviour
 {
     [SerializeField] private PauseScreen _pauseScreen;
     [SerializeField] private MobileInput _mobileInput;
-/* Раскоментируй после создания промо - материала
+
     private void OnApplicationFocus()
     {
         if (_mobileInput.enabled == true)
         {
             _pauseScreen.Open();
         }
-    }*/
+    }
 }
